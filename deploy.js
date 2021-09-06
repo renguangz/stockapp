21f7794ef8207eb9884d77491c1e3b6bbb41a06d
renguangz <rg870328@gmail.com>
2021-09-07 00:32:25 +0800

    deploy.js mistake

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ const routes = [
 ]
 
 const fs = require('fs-extra')
-const path = requeire('path')
+const path = require('path')
 routes.forEach(route => {
     fs.copySync(path.join('build', 'index.html'), path.join('build', route, 'index.html'))
 })
