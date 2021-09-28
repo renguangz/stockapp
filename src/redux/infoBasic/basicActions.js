fc97b62401ef1dc07ae303641e0afe624a8f6f0b
renguangz <rg870328@gmail.com>
2021-09-28 10:59:22 +0800

    async await to promise all

--- src/redux/infoBasic/basicActions.js
+++ src/redux/infoBasic/basicActions.js
@@ -22,39 +22,32 @@ export const fetchBasicError = error => {
     }
 }
 
-export const fetchBasicIncome = stockid => dispatch => {
-    dispatch(fetchBasicRequest)
-}
-
 export const fetchBasic = stockid => {
-    return async (dispatch) => {
-        dispatch(fetchBasicRequest)
-        const fetchBalance = await fetch('/balance', {
-            method: 'POST',
-            body: JSON.stringify({
-                'table_name': stockid
-            })
-        }).then(res => res.json()).then(data => {
-            return data
-        }).catch(err => dispatch(fetchBasicError(err)))
-
-        const fetchIncome = await fetch('/income', {
-            method: 'POST',
-            body: JSON.stringify({
-                'table_name': stockid
-            })
-        }).then(res => res.json()).then(data => {
-            return data
-        }).catch(err => dispatch(fetchBasicError(err)))
-
-        const fetchCashFlow = await fetch('/cashFlow',{
-            method: 'POST',
-            body: JSON.stringify({
-                'table_name': stockid
-            })
-        }).then(res => res.json()).then(data => {
-            return data
-        }).catch(err => dispatch(fetchBasicError(err)))
-        dispatch({ type: 'FETCH_BASIC_SUCCESS', payload: { income: fetchIncome, balance: fetchBalance, cashFlow: fetchCashFlow } })
+    return (dispatch) => {
+        // dispatch(fetchBasicRequest)
+        Promise.all(
+            [
+                fetch('/income', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'table_name': stockid
+                    })
+                }).then(res => res.json()),
+                fetch('/balance', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'table_name': stockid
+                    })
+                }).then(res => res.json()),
+                fetch('/cashFlow', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'table_name': stockid
+                    })
+                }).then(res => res.json())
+            ]
+        ).then(([income, balance, cashFlow]) => {
+            dispatch({ type: 'FETCH_BASIC_SUCCESS', payload: { income, balance, cashFlow } })
+        })
     }
 }
--- src/redux/topSellBuy/topSellBuyActions.js
+++ src/redux/topSellBuy/topSellBuyActions.js
@@ -23,25 +23,42 @@ export const fetchSellBuyError = error => {
 
 export const fetchSellBuy = stockid => {
     return async (dispatch) => {
-        dispatch(fetchSellBuyRequest)
-        const fetchTopBuy = await fetch('/topBuy', {
-            method: 'POST',
-            body: JSON.stringify({
-                'table_name': stockid
-            })
-        }).then(res => res.json()).then(data => {
-            return data
-        }).catch(err => dispatch(fetchSellBuyError(err)))
+        Promise.all(
+            [
+                fetch('/topBuy', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'table_name': stockid
+                    })
+                }).then(res => res.json()),
+                fetch('/topSell', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'table_name': stockid
+                    })
+                }).then(res => res.json())
+            ]
+        ).then(([topBuy, topSell]) => {
+            dispatch({ type: 'FETCH_SELLBUY_SUCCESS', payload: { topBuy, topSell } })
+        })
+        // const fetchTopBuy = await fetch('/topBuy', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         'table_name': stockid
+        //     })
+        // }).then(res => res.json()).then(data => {
+        //     return data
+        // }).catch(err => dispatch(fetchSellBuyError(err)))
 
-        const fetchTopSell = await fetch('/topSell', {
-            method: 'POST',
-            body: JSON.stringify({
-                'table_name': stockid
-            })
-        }).then(res => res.json()).then(data => {
-            return data
-        }).catch(err => dispatch(fetchSellBuyError(err)))
+        // const fetchTopSell = await fetch('/topSell', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         'table_name': stockid
+        //     })
+        // }).then(res => res.json()).then(data => {
+        //     return data
+        // }).catch(err => dispatch(fetchSellBuyError(err)))
 
-        dispatch({ type: 'FETCH_SELLBUY_SUCCESS', payload: { topBuy: fetchTopBuy, topSell: fetchTopSell } })
+        // dispatch({ type: 'FETCH_SELLBUY_SUCCESS', payload: { topBuy: fetchTopBuy, topSell: fetchTopSell } })
     }
 }
